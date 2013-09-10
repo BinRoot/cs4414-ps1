efb66412748229014adf251019da354a6b5a4a2a
UVa student <student@student-VirtualBox.(none)>
2013-09-10 19:31:20 -0400

    server working

--- zhttpto.rs
+++ zhttpto.rs
@@ -9,7 +9,7 @@ extern mod extra;
 
 use extra::uv;
 use extra::{net_ip, net_tcp};
-use std::str;
+use std::{str, os, io, path};
 
 static BACKLOG: uint = 5;
 static PORT:    uint = 4414;
@@ -38,18 +38,69 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
                         let request_str = str::from_bytes(bytes.slice(0, bytes.len() - 1));
                         println(fmt!("Request received:\n%s", request_str));
                         visitor_count += 1;
-                        let response: ~str = ~
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-                             <doctype !html><html><head><title>Hello, Rust!</title>
-                             <style>body { background-color: #111; color: #FFEEAA }
-                                    h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                             </style></head>
-                             <body>
-                             <h1>Greetings, Rusty!</h1><center>"
-                             + visitor_count.to_str() +
-                             "</center></body></html>\r\n";
 
-                        net_tcp::write(&sock, response.as_bytes_with_null_consume());
+                        let mut ls = ~[];
+                        for request_str.line_iter().advance | l | { ls.push(l); }
+                        let first_line = ls[0];
+
+                        let mut ws = ~[];
+                        for first_line.split_iter(' ').advance | w | { ws.push(w); }
+                        let mut full_path = ~"";
+                        let mut root = false;
+                        if ws.len() == 3 {
+                            let getExists = ws[0]=="GET";
+                            let httpExists = ws[2].starts_with("HTTP/1.1");
+                            if (getExists && httpExists) {
+                                println(os::getcwd().to_str() + ws[1]);
+                                full_path = os::getcwd().to_str() + ws[1];
+                                if ws[1] == "/" {
+                                    println("at root");
+                                    root = true;
+                                }
+                            }
+                        }
+
+                        let read_result = io::file_reader(~path::Path(full_path));
+                        let mut alt_resp = ~"";
+                        
+                        if !root {
+                            if read_result.is_ok() {
+                                let file = read_result.unwrap();
+                                let line_arr = file.read_lines();
+                                println(line_arr.to_str());
+                                
+                                let mut i = 0;
+                                let len_lines = line_arr.len();
+                                println("len lines: " + len_lines.to_str());
+                                while i<len_lines {
+                                    println(i.to_str()+": "+line_arr[i]);
+                                    alt_resp = alt_resp + line_arr[i] + "\r\n";
+                                    i += 1;
+                                }
+                            }
+                        }
+                        
+                        println("alt_resp: " + alt_resp);
+                        
+                        if alt_resp==~"" {
+                            let response: ~str = ~
+                                "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                                <doctype !html><html><head><title>Hello, Rust!</title>
+                                <style>body { background-color: #111; color: #FFEEAA }
+                                h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
+                                </style></head>
+                                <body>
+                                <h1>Greetings, Rusty!</h1><center>"
+                                + visitor_count.to_str() +
+                                "</center></body></html>\r\n";
+
+                            net_tcp::write(&sock, response.as_bytes_with_null_consume());
+                        } else {
+                            let response: ~str = ~
+                                "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+                                + alt_resp;
+                            net_tcp::write(&sock, response.as_bytes_with_null_consume());
+                        }
                     },
                 };
             }
